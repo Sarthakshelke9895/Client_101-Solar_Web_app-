aa6c67b669efa111edb792c867eeeb1a2d9cf72d
Dell <sarthak shelke>
2025-01-18 21:33:35 +0530

    Navbar making in progress1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
-import './App.css';
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Services = () => <div>Services Page</div>;
+const Contact = () => <div>Contact Page</div>;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello Ballya</h1>
+    <div className='container'>
+          <Router>
+      <Navbar />
+      <div style={{ marginTop: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
+
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+    {/* Left section: User Login */}
+    <div className="navbar__login">
+      <span className="navbar__user-name">Hello, John</span>
+    </div>
+
+    {/* Right section: Navigation Links */}
+    <ul className="navbar__links">
+      <li className="navbar__item">
+        <Link to="/" className="navbar__link">Home</Link>
+      </li>
+      <li className="navbar__item">
+        <Link to="/about" className="navbar__link">About</Link>
+      </li>
+      <li className="navbar__item">
+        <Link to="/services" className="navbar__link">Services</Link>
+      </li>
+      <li className="navbar__item">
+        <Link to="/contact" className="navbar__link">Contact</Link>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Navbar
